d9e6cb938c1630225620ab466de2fd98cbf752bc
KGarvey92 <kgarvey92@gmail.com>
2022-08-12 16:02:21 +0800

    fix persistant calculations

--- script.js
+++ script.js
@@ -8,36 +8,49 @@ let display = document.querySelector('#display');
 // initialise variable to check which operator has been selected
 let selectedOperator = null;
 
+// intialise boolean to check whether a calculation is ongoing
+let ongoingCalc = false;
+
 //function that performs operation
 function operate(operator, a, b) {
     if (operator === "*") {
         valueA = a * b;
         display.textContent = valueA;
         valueB = null;
+        ongoingCalc = true;
     }
     else if (operator === "/") {
         valueA = a / b;
         display.textContent = valueA; 
         valueB = null;
+        ongoingCalc = true;
     }
     else if (operator === "+") {
         valueA = a + b;
         display.textContent = valueA;
         valueB = null;
+        ongoingCalc = true;
     }
     else {
         valueA = a - b;
         display.textContent = valueA;
         valueB = null;
+        ongoingCalc = true;
     }
 }
 
 // function that changes display value and alter number variables.
 
 function enterNumber(num) {
+
     if (valueA === 0 && selectedOperator === null) {
-        display.textContent = num;
         valueA = num; 
+        display.textContent = valueA;
+    }
+    else if (ongoingCalc === true && selectedOperator === null) {
+        ongoingCalc = false;
+        valueA = num;
+        display.textContent = valueA;
     }
     else if (selectedOperator === null) {
         valueA += num;
@@ -56,19 +69,32 @@ function enterNumber(num) {
 // event listeners for number buttons
 document.querySelectorAll(".numberBtn").forEach(btn => btn.addEventListener('click', () => {
     enterNumber(btn.textContent);
-}))
+}));
 
-// TODO: event listener for C
-    
-
-// TODO: event listener for operators
+// event listeners for operators
 document.querySelectorAll(".operatorBtn").forEach(btn => btn.addEventListener('click', () => {
     // check if valueB and selectedOperator have values
     if (valueB && selectedOperator){
         //call operation function
         operate(selectedOperator, Number(valueA), Number(valueB));
-
     }
     //update selectedOperator
     selectedOperator = btn.value;
 }));
+
+// TODO: event listener for C
+document.querySelector("#clearBtn").addEventListener('click', () => {
+    if (valueB && selectedOperator){
+        //call operation function
+        operate(selectedOperator, Number(valueA), Number(valueB));
+    }
+});
+
+// TODO: event listener for equals
+document.querySelector("#equalsBtn").addEventListener('click', () => {
+    if (valueB && selectedOperator){
+        //call operation function
+        operate(selectedOperator, Number(valueA), Number(valueB));
+        selectedOperator = null;
+    }
+});
